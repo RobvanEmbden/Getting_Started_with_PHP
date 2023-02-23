25fdd43e1aa99d220c2200666661311b2d344f48
Rob van Embden <61789232+RobvanEmbden@users.noreply.github.com>
2023-02-23 18:23:45 +0100

    v1

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE>
+<html>
+
+<head>
+    <title>Rob van Embden PHP</title>
+</head>
+
+<body>
+    <h1> Rob van Embden PHP<h1>
+            <p style="font-size: medium; font-weight: 200;">
+                The SHA256 hash of "Rob van Embden" is <?php echo hash('sha256', 'Rob van Embden'); 
+    
+    ?>
+            </p>
+            <p>
+            <pre style="font-size: medium; font-weight: 200;">ASCII Art:
+
+         
+        *********
+        **      **
+        **     **
+        *** ***
+        **    **            
+        **     **
+        **       **
+
+
+    </pre>
+            </p>
+            <p style="font-size: medium; font-weight: 200;">
+                <a href="check.php">
+                    Click here to check the error setting
+                </a>
+                <br>
+                <a href="fail.php">
+                    Click here to cause a traceback
+                </a>
+            </p>
+</body>
+
+</html>
